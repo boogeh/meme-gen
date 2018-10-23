ead1898e852b537ddc97e77df6b1581688c77d9d
boogeh <wagner.amit@gmail.com>
2018-10-23 16:39:52 +0300

    pushing canvas width height

--- js/canvas-service.js
+++ js/canvas-service.js
@@ -4,7 +4,7 @@ var gCanvas;
 var gCtx;
 
 function setCanvas() {
-    var elCanvasDiv = document.querySelector('.canvas-container')
+    var elCanvasDiv = document.querySelector('.canvas-wrapper')
     gCanvas = document.querySelector('#main-canvas')
     gCanvas.width = elCanvasDiv.clientWidth; // canvas width same as img width
     gCtx = gCanvas.getContext('2d')
