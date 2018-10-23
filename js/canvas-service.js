8c998a6188bdd49a9892231f55a6ddff432eaae9
elad <eladozy@gmail.com>
2018-10-23 14:03:46 +0300

    added canvas control

--- js/canvas-service.js
+++ js/canvas-service.js
@@ -5,9 +5,11 @@ var gCtx;
 
 function setCanvas() {
     gCanvas = document.querySelector('#main-canvas')
+    gCanvas.style.width ='100%';
+    // gCanvas.style.width ='';
     gCtx = gCanvas.getContext('2d')
 }
-
+ 
 function drawImage(img) {
     console.log(img)
     gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
