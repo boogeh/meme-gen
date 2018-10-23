c1b965e4bbb3fe81daa15185d2588b3ff6fed95b
boogeh <wagner.amit@gmail.com>
2018-10-23 15:37:29 +0300

    fixed canvas height & width by screen size

--- js/canvas-service.js
+++ js/canvas-service.js
@@ -6,14 +6,13 @@ var gCtx;
 function setCanvas() {
     var elCanvasDiv = document.querySelector('.canvas-container')
     gCanvas = document.querySelector('#main-canvas')
-    gCanvas.width = elCanvasDiv.width;
+    gCanvas.width = elCanvasDiv.clientWidth; // canvas width same as img width
     gCtx = gCanvas.getContext('2d')
 }
- 
-function drawImage(img) {
-    console.log(img)
-    gCtx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
-                    0, 0, gCanvas.width, gCanvas.height); 
 
-    // gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+function drawImage(img) {
+    // canvas height same as img height
+    gCanvas.height = img.clientHeight
+    gCtx.drawImage(img, 0, 0, img.width, img.height,     // source rectangle
+                        0, 0, gCanvas.width, gCanvas.height);
 }
