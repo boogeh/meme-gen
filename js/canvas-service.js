0807bb985a117860c3da3fab3e3e3581890231d9
elad <eladozy@gmail.com>
2018-10-23 14:16:57 +0300

    changed colntrollers

--- js/canvas-service.js
+++ js/canvas-service.js
@@ -5,12 +5,15 @@ var gCtx;
 
 function setCanvas() {
     gCanvas = document.querySelector('#main-canvas')
-    gCanvas.style.width ='100%';
+    gCanvas.width ='100%';
     // gCanvas.style.width ='';
     gCtx = gCanvas.getContext('2d')
 }
  
 function drawImage(img) {
     console.log(img)
-    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    gCtx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
+        0, 0, gCanvas.width, gCanvas.height); 
+
+    // gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
 }
